9700313deceeec3272d864cf4f8a8028c20fe902
Shubhangichavan42 <chavanshubhangi191@gmail.com>
2026-03-14 17:52:35 +0530

    Intial Setup Done

---
+++ src/main/java/com/rahul/projectevaulation/ProjectEvaulationApplication.java
@@ -0,0 +1,13 @@
+package com.rahul.projectevaulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEvaulationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectEvaulationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rahul/projectevaulation/ProjectEvaulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.projectevaulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEvaulationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
